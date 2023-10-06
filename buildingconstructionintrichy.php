0eae87b77707735c87507988f4871115f76a711d
Harish-55 <harishcs2002@gmail.com>
2023-10-06 15:58:18 +0530

    Correction Finished

---
+++ buildingconstructionintrichy.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Verna Engineers & Contractors</title>
+    <link rel="stylesheet preload" as="style" href="css/preload.min.css">
+    <link rel="stylesheet preload" as="style" href="css/icomoon.css">
+    <link rel="stylesheet preload" as="style" href="css/libs.min.css">
+    <link rel="stylesheet" href="css/gallery-grid.min.css">
+    <link rel="stylesheet" href="css/floatbutton.min.css">
+    <link rel=stylesheet href=css/gallery-masonry.min.css>
+     <link rel=stylesheet href=css/single-post.min.css>
+    <link rel="icon" type="image/x-icon" href="img/logo/fav-icon.png">
+</head>
+
+<body>
+    <header class="header" data-page="home" data-page-parent="home">
+        <div class="container d-flex">
+            <a class="brand" href="#">
+                <img class="brand_logo" src="img/logo/logo.png" alt="Verna Engineers & Contractors" />
+            </a>
+            <nav class="header_nav collapse" id="headerMenu">
+                <ul class="header_nav-list">
+                    <li class="header_nav-list_item">
+                        <a class="nav-link nav-item" href="index.php" data-page="home" id="links"><span
+                                class="nav-item_text">Home
+                            </span></a>
+                    </li>
+                    <li class="header_nav-list_item">
+                        <a class="nav-item nav-link" href="about.php" data-page="about" data-single="true"
+                            id="links"><span class="nav-item_text">About</span></a>
+                    </li>
+                    <li class="header_nav-list_item">
+                        <a class="nav-link nav-item" data-page="services" data-page-parent="services" href="service.php"
+                            data-main="true" id="links">Services</a>
+                    </li>
+                    <li class="header_nav-list_item">
+                        <a class="nav-link nav-item" data-page="projects2" data-page-parent="projects"
+                            href="projects.php" id="links">Projects</a>
+                    </li>
+                    <li class="header_nav-list_item">
+                        <a class="nav-link nav-item" data-page="gallery-grid" data-page-parent="pages"
+                            href="gallery.php" id="links">Gallery</a>
+                    </li>
+                    <li class="header_nav-list_item">
+                        <a class="dropdown-item nav-item" data-page="contacts2" data-page-parent="contacts"
+                            href="contacts.php" id="links">Contact us</a>
+                    </li>
+                </ul>
+            </nav>
+            <button class="header_trigger" data-bs-toggle="collapse" data-bs-target="#headerMenu"
+                aria-controls="headerMenu" aria-expanded="false">
+                <span class="line line--short"></span> <span class="line"></span>
+                <span class="line line--short"></span> <span class="line"></span>
+            </button>
+        </div>
+    </header>
+
+    <header class="page primary-bg">
+        <div class=container>
+            <div class=section_header><span class="subtitle subtitle--extended">Verna Engineers & Construction</span>
+                <h1 class=title>Building construction in Trichy</h1>
+                <ul class=breadcrumbs>
+                    <li><a href=index.php>Home</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class=media>
+            <picture>
+                <source data-srcset=img/plan.webp srcset=img/plan.webp type=image/webp><img class=lazy
+                    data-src=img/plan.png src=img/plan.png alt=media>
+            </picture>
+        </div>
+    </header>
+    <main class="post section-nopb">
+        <div class="post_container container">
+            <div class=post_content>
+                <article class=post_article>
+                    <h3 class=post_article-header>Excellence and innovation built into every design</h3>
+                    <p class=post_article-text>Imperdiet dui accumsan sit amet <a class=post_article-link href=#>link
+                            style</a> nulla facilisi morbi. Bibendum enim facilisis gravida neque convallis a cras
+                        semper. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium.
+                        Mauris nunc congue nisi vitae. Tortor condimentum lacinia quis vel eros donec. Nibh sit amet
+                        commodo nulla facilisi nullam vehicula.</p>
+                    <div class=block>
+                        <ul class=checklist>
+                            <li class=checklist_item><i class="icon-check icon"></i> Building the future with ideas</li>
+                            <li class=checklist_item><i class="icon-check icon"></i> Designing future with excellence
+                            </li>
+                            <li class=checklist_item><i class="icon-check icon"></i> Discovering possibility in concrete
+                            </li>
+                            <li class=checklist_item><i class="icon-check icon"></i> Engineering the better way</li>
+                            <li class=checklist_item><i class="icon-check icon"></i> Purus sit amet volutpat</li>
+                        </ul>
+                        <div class=media>
+                            <picture>
+                                <source data-srcset=img/blog/01.webp srcset=img/blog/01.webp type=image/webp><img
+                                    class=lazy data-src=img/blog/01.jpg src=img/blog/01.jpg alt=media>
+                            </picture>
+                        </div>
+                        </div>
+                        <p class=post_article-text>Facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris. In
+                        aliquam sem fringilla ut morbi tincidunt augue interdum. Quam id leo in vitae turpis massa sed
+                        elementum. Id consectetur purus ut faucibus pulvinar elementum integer enim. Sapien eget mi
+                        proin sed libero enim sed faucibus</p>
+                    
+                </article>
+            </div>
+        </div>
+    </main>
+    <br>
+    
+    <footer class="footer primary-bg">
+        <div class="container">
+            <div class="footer_main">
+                <div class="footer_main-block">
+                    <a class="brand" href="index.php"><img class="brand_logo" src="img/logo/logo.png"
+                            alt="Axial Construct" /></a>
+                    <p class="footer_main-block_subtitle
+                            footer_main-block_subtitle--brand">Verna Engineering
+                        is the one-stop-solution for all of your land
+                        development needs. If you are struggling to bring
+                        your dreams to life, look no further! Proud member of Builder association of india (BAI)</p>
+                </div>
+                <div class="footer_main-block">
+                    <h4 class="footer_main-block_title">Quick Link</h4>
+                    <ul class="footer_main-block_nav">
+                        <li class="list-item">
+                            <a class="nav-link link" href="about.php" data-page="about"><i
+                                    class="icon-chevron_right icon"></i>
+                                About</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="service.php" data-page="services"><i
+                                    class="icon-chevron_right icon"></i>
+                                Services</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="projects.php" data-page="projects"><i
+                                    class="icon-chevron_right icon"></i>
+                                Projects</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="gallery.php" data-page="blog"><i
+                                    class="icon-chevron_right icon"></i>
+                                Gallery</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="contacts.php" data-page="blog"><i
+                                    class="icon-chevron_right icon"></i>
+                                Contact Us</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="privacy_policy.php" data-page="blog"><i
+                                    class="icon-chevron_right icon"></i>
+                                Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer_main-block">
+                    <h4 class="footer_main-block_title">Services</h4>
+                    <ul class="footer_main-block_nav">
+                        <li class="list-item">
+                            <a class="nav-link link" href="service.php" data-page="about"><i
+                                    class="icon-chevron_right icon"></i>
+                                Licensed Surveyor</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="service.php" data-page="services"><i
+                                    class="icon-chevron_right icon"></i>
+                                Renovation Contractor</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="service.php" data-page="blog"><i
+                                    class="icon-chevron_right icon"></i>
+                                Interior Designer</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="service.php" data-page="services"><i
+                                    class="icon-chevron_right icon"></i>
+                                Real Estate</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="service.php" data-page="projects"><i
+                                    class="icon-chevron_right icon"></i>
+                                Building Contractor</a>
+                        </li>
+                        <li class="list-item">
+                            <a class="nav-link link" href="service.php" data-page="blog"><i
+                                    class="icon-chevron_right icon"></i>
+                                Promoter</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer_main-block">
+                    <h4 class="footer_main-block_title">Contact</h4>
+                    <div class="group-wrapper">
+                        <i class="icon-location icon"></i>
+                        <div class="group text-justify">
+                            <span>No. 3, B. P Complex, Opp. Velammal bodhi campus, Chinthamani nagar, Olaiyur main road,
+                                Udayanpatti, Trichy - 21</span>
+                        </div>
+                    </div>
+                    <div class="group-wrapper" style="margin-bottom: 20px;">
+                        <i class="icon-call icon"></i>
+                        <div class="group">
+                            <a href="tel:+919043010199" style="margin-bottom: 5px;">+91 90430 10199</a>
+                            <a href="tel:+917904678950">+91 79046 78950</a>
+                        </div>
+                    </div>
+                    <div class="group-wrapper">
+                        <i class="icon-call icon"></i>
+                        <div class="group">
+                            <a href="mailto:info@vernaconstruction.com"
+                                style="margin-bottom: 5px;">info@vernaconstruction.com</a>
+                            <a href="mailto:vernaconstruction@gmail.com">vernaconstruction@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_secondary">
+                <p class="footer_secondary-copyright">
+                    <span>&copy; <span id="currentYear"></span> <a href="https://www.zworktechnology.com/">Zwork
+                            Technologies</a></span>
+                </p>
+                <ul class="socials">
+                    <li class="socials_item">
+                        <a class="socials_item-link" href="https://www.facebook.com/verna.engineers.contractors"
+                            target="_blank" rel="noopener noreferrer" aria-label="Facebook"><i
+                                class="icon-facebook"></i></a>
+                    </li>
+                    <li class="socials_item">
+                        <a class="socials_item-link" href="https://www.instagram.com/verna_engineers_contractors/"
+                            target="_blank" rel="noopener noreferrer" aria-label="Instagram"><i
+                                class="icon-instagram"></i></a>
+                    </li>
+                    <li class="socials_item">
+                        <a class="socials_item-link" href="https://api.whatsapp.com/send/?phone=%2B919043010199"
+                            target="_blank" rel="noopener noreferrer" aria-label="WhatsApp"><i
+                                class="icon-whatsapp"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <button id="scrollTrigger" type="button"><i class="icon
+                icon-arrow_right"></i></button>
+    <script id="www-widgetapi-script" src="../yts/jsbin/www-widgetapi-vflS50iB-/www-widgetapi.js" async=""></script>
+    <script src="../player_api"></script>
+    <script src="js/common.min.js"></script>
+    <script src="js/demo.min.js"></script>
+    <script async="" src="../gtag/js?id=undefined"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'undefined');
+    </script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet preload" as="style" href="css/icomoon.css" />
     <link rel="stylesheet preload" as="style" href="css/libs.min.css" />
     <link rel="stylesheet" href="css/index.min.css" />
+    <link rel="stylesheet" href="css/index2.min.css" />
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/floatbutton.min.css" />
     <link rel="icon" type="image/x-icon" href="img/logo/fav-icon.png">
@@ -57,7 +58,82 @@
             </button>
         </div>
     </header>
-    <main>
+    
+<main> 
+    <section class="about primary-bg section">
+            <div class=container>
+                <div class="about_header section_header"><span class=subtitle data-aos=fade-down>Just feel the
+                        future</span>
+                    <h2 class=title data-aos=fade-right>Bringing Your Ideas <br> and Innovations to Life</h2>
+                    <p class=text data-aos=fade-in data-aos-duration=700>Vitae ultricies leo integer malesuada nunc vel.
+                        Molestie ac <br> feugiat sed lectus vestibulum mattis ullamcorper velit. Viverra <br> aliquet eget sit
+                        amet tellus cras adipiscing</p>
+                    <ul class=achievements>
+                        <li class=achievements_item data-aos=fade-up><svg class=icon width=40 height=40
+                                viewBox="0 0 40 40" fill=none xmlns=http://www.w3.org/2000/svg>
+                                <g clip-path=url(#clip01)>
+                                    <path
+                                        d="M36.4062 6.5625L21.4062 0.3125C20.9375 0.15625 20.4688 0.078125 20 0.078125C19.4531 0.078125 18.9844 0.15625 18.5156 0.3125L3.51562 6.5625C2.10938 7.1875 1.25 8.51562 1.25 10C1.25 25.5469 10.1562 36.25 18.5156 39.7656C19.4531 40.1562 20.4688 40.1562 21.4062 39.7656C28.125 36.9531 38.75 27.3438 38.75 10C38.75 8.51562 37.8125 7.1875 36.4062 6.5625ZM20.4688 37.4219C20.1562 37.5781 19.7656 37.5781 19.4531 37.4219C11.875 34.375 3.75 23.75 3.75 10C3.75 9.53125 3.98438 9.0625 4.45312 8.90625L19.4531 2.65625C19.7656 2.5 20.1562 2.5 20.4688 2.65625L35.4688 8.90625C35.9375 9.0625 36.25 9.53125 36.1719 10C36.25 23.75 28.125 34.375 20.4688 37.4219ZM31.0938 12.0312C30.7031 11.7188 30.1562 11.7188 29.7656 12.0312L17.0312 24.6875L11.6406 19.2969C11.25 18.9062 10.625 18.9062 10.3125 19.2969L9.60938 19.9219C9.21875 20.3125 9.21875 20.8594 9.60938 21.25L16.3281 28.0469C16.7188 28.3594 17.2656 28.3594 17.6562 28.0469L31.7188 14.0625C32.1094 13.6719 32.1094 13.125 31.7188 12.7344L31.0938 12.0312Z"
+                                        fill=#0DA574 />
+                                </g>
+                                <defs>
+                                    <clipPath id=clip01>
+                                        <rect width=40 height=40 fill=white />
+                                    </clipPath>
+                                </defs>
+                            </svg> <span class=label>Successfully Completed Projects</span></li>
+                        <li class=achievements_item data-aos=fade-up data-aos-delay=50><svg class=icon width=41
+                                height=40 viewBox="0 0 41 40" fill=none xmlns=http://www.w3.org/2000/svg>
+                                <g clip-path=url(#clip02)>
+                                    <path
+                                        d="M39.4688 28.8281L29.8594 19.2188L33.7656 15.3125L39.0781 10C40.5625 8.51562 40.5625 6.17188 39.0781 4.6875L35.5625 1.17188C34.8594 0.390625 33.9219 0 32.9062 0C31.9688 0 31.0312 0.390625 30.25 1.17188L24.9375 6.48438L21.0312 10.3906L11.4219 0.78125C10.9531 0.3125 10.3281 0 9.70312 0C9.07812 0 8.45312 0.3125 7.98438 0.78125L0.953125 7.73438C-0.0625 8.75 -0.0625 10.3125 0.953125 11.25L10.5625 20.8594L1.73438 29.6875L0.25 38.125C0.015625 39.2969 1.10938 40.2344 2.125 40L10.5625 38.5156L19.3906 29.6875L29 39.2969C29.3906 39.7656 30.0156 40 30.7188 40C31.3438 40 31.9688 39.7656 32.4375 39.2969L39.4688 32.2656C40.4844 31.3281 40.4844 29.7656 39.4688 28.8281ZM32.0469 2.89062C32.3594 2.57812 33.4531 2.57812 33.8438 2.89062L37.3594 6.40625C37.8281 6.95312 37.8281 7.73438 37.3594 8.20312L33.7656 11.7969L28.4531 6.48438L32.0469 2.89062ZM2.67188 9.53125L9.70312 2.57812L14.0781 6.875L11.0312 10C10.7969 10.2344 10.7969 10.625 11.0312 10.8594L11.8906 11.7188C12.125 11.9531 12.5156 11.9531 12.75 11.7188L15.875 8.67188L19.3125 12.1094L12.2812 19.1406L2.67188 9.53125ZM9.3125 36.25L2.90625 37.3438L4 30.9375L26.6562 8.28125L31.9688 13.5938L9.3125 36.25ZM30.7188 37.5781L21.1094 27.9688L28.1406 20.9375L31.5781 24.375L28.5312 27.5C28.2969 27.7344 28.2969 28.125 28.5312 28.3594L29.3906 29.2188C29.625 29.4531 30.0156 29.4531 30.25 29.2188L33.375 26.1719L37.75 30.5469L30.7188 37.5781Z"
+                                        fill=#0DA574 />
+                                </g>
+                                <defs>
+                                    <clipPath id=clip02>
+                                        <rect width=40 height=40 fill=white transform=translate(0.25) />
+                                    </clipPath>
+                                </defs>
+                            </svg> <span class=label>Simplicity In Design With Practically In Mind</span></li>
+                        <li class=achievements_item data-aos=fade-up data-aos-delay=100><svg class=icon width=41
+                                height=40 viewBox="0 0 41 40" fill=none xmlns=http://www.w3.org/2000/svg>
+                                <g clip-path=url(#clip04)>
+                                    <path
+                                        d="M37.75 27.5V25C37.75 17.1094 32.4375 10.4688 25.25 8.35938V7.5C25.25 6.17188 24.0781 5 22.75 5H17.75C16.3438 5 15.25 6.17188 15.25 7.5V8.35938C7.98438 10.4688 2.75 17.1094 2.75 25V27.5C1.34375 27.5 0.25 28.6719 0.25 30V32.5C0.25 33.9062 1.34375 35 2.75 35H37.75C39.0781 35 40.25 33.9062 40.25 32.5V30C40.25 28.6719 39.0781 27.5 37.75 27.5ZM5.25 25C5.25 18.9062 9 13.6719 14.3125 11.4062L16.3438 19.5312C16.4219 19.8438 16.6562 20 17.0469 20C17.3594 20 17.6719 19.6875 17.75 19.2969V7.5H22.75V19.2969C22.75 19.6875 23.0625 20 23.375 20C23.7656 20 24 19.8438 24.0781 19.5312L26.1094 11.4062C31.4219 13.6719 35.25 18.9062 35.25 25V27.5H5.25V25ZM37.75 32.5H2.75V30H37.75V32.5Z"
+                                        fill=#0DA574 />
+                                </g>
+                                <defs>
+                                    <clipPath id=clip04>
+                                        <rect width=40 height=40 fill=white transform=translate(0.25) />
+                                    </clipPath>
+                                </defs>
+                            </svg> <span class=label>High Level of Construction Control Approval</span></li>
+                    </ul><br><span class="wrapper d-block" data-aos=fade-right data-aos-delay=50><a class=btn href=#>About
+                            company</a></span>
+                </div>
+                <div class=secondary>
+                    <!-- <ul class=numbers_list>
+                        <li class=numbers_list-item>
+                            <h2 class="countNum number highlight" data-value=200 data-suffix=+>0</h2><span
+                                class=label>Active Projects in Civil Engineering</span>
+                        </li>
+                        <li class=numbers_list-item>
+                            <h2 class="countNum number highlight" data-value=99 data-suffix=%>0</h2><span
+                                class=label>Building Control Approval Rate</span>
+                        </li>
+                    </ul> -->
+                    <div class=media data-aos=zoom-in data-aos-duration=600>
+                        <picture>
+                            <source data-srcset="img/bg/1.png" srcset="img/bg/1.png" type=image/webp><img
+                                class=lazy data-src="img/bg/1.png" src="img/bg/1.png" alt=media>
+                        </picture>
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+    
+    
         <section class="hero primary-bg">
             <div class="hero_media" data-aos="fade-up" data-aos-delay="50">
                 <picture>
@@ -835,13 +911,13 @@
                 </div>
             </div>
         </div>
-        <div class="iklan">
+        <!-- <div class="iklan">
             <p style="font-size: 16px; font-weight: 400;">For latest related updates follow us now.</p>
             <a href="https://www.instagram.com/verna.engineers.contractors/" target="_blank"><button
                     style="background-color: #129cf7; font-size: 14px; padding: 10px; margin-top:8px; font-weight: 600; border-radius: .25rem; color: white;"><span>Facebook</span></button></a>
             <a href="https://www.instagram.com/verna_engineers_contractors/" target="_blank"><button
                     style="background-color: #ff3040; font-size: 14px; padding: 10px; margin-top:8px; font-weight: 600; border-radius: .25rem; color: white;"><span>Instagram</span></button></a>
-        </div>
+        </div> -->
     </main>
     <footer class="footer primary-bg">
         <div class="container">
